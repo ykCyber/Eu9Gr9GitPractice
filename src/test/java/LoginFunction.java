a4bffb575c63306fd77539837c6cda80ba3822ee
huseyinboncuk <huseyinboncuk@hotmail.com>
2022-07-25 21:19:23 +0300

    today is off

---
+++ src/test/java/LoginFunction.java
@@ -0,0 +1,9 @@
+public class LoginFunction {
+
+    public static void main(String[] args) {
+
+        System.out.println("Login is working");
+
+        System.out.println("today I m off");
+    }
+}
